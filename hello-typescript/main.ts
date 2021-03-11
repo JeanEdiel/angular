aeb93d6553e7b2c89ce1a18ed358501498d8575a
Jean Mello <mello.jeanediel@hotmail.com>
2021-03-11 19:54:59 -0300

    second commit

---
+++ hello-typescript/main.ts
@@ -0,0 +1,24 @@
+var minhavar = 'minha variavel';
+
+function minhaFunc(x, y) {
+    return x + y;
+}
+
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map((valor) => {
+    return valor * 2;
+});
+
+class Matematica
+{
+    soma(x, y)
+    {
+        return x + y;
+    }
+}
+
+var n1: string = 'aasdasd';
+
